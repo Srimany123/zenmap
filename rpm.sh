61bba104ab77b851a3cbad0f0f7dd31f7b8d25c7
Srimany123 <58987560+Srimany123@users.noreply.github.com>
2022-12-14 22:20:22 +0530

    Create rpm.sh

---
+++ rpm.sh
@@ -0,0 +1,5 @@
+sudo apt-get install rpm
+sudo rpm -vhU https://nmap.org/dist/nmap-7.92-1.x86_64.rpm
+sudo rpm -vhU https://nmap.org/dist/zenmap-7.92-1.noarch.rpm
+sudo rpm -vhU https://nmap.org/dist/ncat-7.92-1.x86_64.rpm
+sudo rpm -vhU https://nmap.org/dist/nping-0.7.92-1.x86_64.rpm
