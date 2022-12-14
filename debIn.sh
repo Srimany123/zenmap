c3aa0a4019b195e7f5db08e3028daf920e26f57f
Srimany123 <58987560+Srimany123@users.noreply.github.com>
2022-12-14 22:19:24 +0530

    Create debIn.sh

---
+++ debIn.sh
@@ -0,0 +1,4 @@
+sudo apt install ./python-cairo_1.16.2-2ubuntu2_amd64.deb
+sudo apt install ./python-gobject-2_2.28.6-14ubuntu1_amd64.deb
+sudo apt install ./python-gtk2_2.24.0-5.1ubuntu2_amd64.deb
+./Alien.sh
