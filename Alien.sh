728900ae466a6503d5961d03994665a878a1c0da
Srimany123 <58987560+Srimany123@users.noreply.github.com>
2022-12-14 22:19:58 +0530

    Create Alien.sh

---
+++ Alien.sh
@@ -0,0 +1,6 @@
+sudo apt install alien
+alien --to-deb /path/to/file.rpm
+sudo alien --to-deb zenmap-7.91-1.noarch.rpm
+sudo chmod +777 zenmap_7.91-2_all.deb
+sudo apt install ./zenmap_7.91-2_all.deb 
+./rpm.sh
